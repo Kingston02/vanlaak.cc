ead4b2e6abcb38b14344826e28059fb4c700ba97
Kingston02 <42833641+Kingston02@users.noreply.github.com>
2019-05-14 19:02:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>van Laak</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script|Great+Vibes|Quattrocento+Sans" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-3">
+    <div class="container-fluid">
+        <a href="#" id="vanlaak" class="navbar-brand mr-3">van Laak Design</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="index.php" class="nav-item nav-link active" id="home">Home</a>
+                <a href="#" class="nav-item nav-link" id="service">Services</a>
+                <a href="#" class="nav-item nav-link" id="about">Over ons</a>
+                <a href="#" class="nav-item nav-link" id="contact">Contact</a>
+            </div>
+        </div>
+    </div>    
+</nav>
+<div class="container">
+    <div class="jumbotron">
+        <h1 id="laak">van Laak Designs</h1>
+        <p class="lead">Welkom bij  <a href="vanlaak.cc" target="_blank">vanLaak.cc</a> wij staan 24/7 voor u klaar wij werken hard om uw website klaar te hebben binnen 8 uur. Een website is het belangerijkste deel van een bedrijf, is de website slecht of onproffesioneel dan zal het bedrijf ook minder goed lopen als een werkende proffesionele website. Vraag nu uw offerte aan en weet vandaag nog de prijs van uw toekomstige website.</p>
+        <p><a href="https://www.tutorialrepublic.com" target="_blank" class="btn btn-outline-primary">Vraag offerte aan</a></p>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-lg-4 col-xl-4">
+            <h2>Nummer 1</h2>
+            <p>Wij zijn nummer 1 in het ontwerpen en het programeren van websites.</p>
+            <p><a href="https://www.tutorialrepublic.com/html-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-4">
+            <h2>Wij werken samen.</h2>
+            <p>Sinds 2015 werken wij samen met <a href="websitedesigner.com" target="_blank">WebsiteDesigner</a> hier kunt u eenvoudig een website ontwerp maken en naar gemakkelijk naar ons door sturen. </p>
+            <p><a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-4">
+            <h2>JavaScript</h2>
+            <p>JavaScript is the most popular and widely used client-side scripting language. Our JavaScript tutorials will provide in-depth knowledge of the JavaScript including ES6 features, so that you can create interactive websites.</p>
+            <p><a href="https://www.tutorialrepublic.com/javascript-tutorial/" target="_blank" class="btn btn-success">Learn More &raquo;</a></p>
+        </div>
+
+    </div>
+    <hr>
+    <footer>
+        <div class="row">
+            <div class="col-md-6">
+                <p>Copyright &copy; 2019 van Laak Designs</p>
+            </div>
+            <div class="col-md-6 text-md-right">
+                <a href="#" class="text-dark">Terms of Use</a> 
+                <span class="text-muted mx-2">|</span> 
+                <a href="#" class="text-dark">Privacy Policy</a>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+</body>
+</html>                            
